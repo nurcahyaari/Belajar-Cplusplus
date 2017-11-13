fcb99010b0dd5eeb35894a37bebe992d7d047815
Unknown <nurcahyaari@gmail.com>
2017-11-13 12:47:25 +0700

    penambahan source code latihan latihan

---
+++ source latihan/input_output_sederhana.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main(){
+    system("clear");
+    string input;
+    cout << "Masukan nama panggilan : ";
+    cin >> input;
+    cout << "Nama anda adalah : " << input << endl;
+}
