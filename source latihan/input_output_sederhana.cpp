116a59253429746b71894020729251ddebe5e6f1
Unknown <nurcahyaari@gmail.com>
2017-11-13 12:49:50 +0700

    penambahan komentar

--- source latihan/input_output_sederhana.cpp
+++ source latihan/input_output_sederhana.cpp
@@ -1,12 +1,10 @@
 #include<iostream>
-#include<stdio.h>
 
 using namespace std;
 
 int main(){
-    system("clear");
-    string input;
-    cout << "Masukan nama panggilan : ";
-    cin >> input;
-    cout << "Nama anda adalah : " << input << endl;
+    string input; //pendeklarasian variabel bernama input bertipe data string
+    cout << "Masukan nama panggilan : "; // melakukan output ke layar monitor berupa text "Masukan nama panggilan : "
+    cin >> input; // meminta inputan dari user. kemudian inputan tersebut akan disimpan kedalam variabel input
+    cout << "Nama anda adalah : " << input << endl; // melakukan output program ke layar monitor berupa text "Nama anda adalah : " ditambah variable yang diinputkan tadi
 }
