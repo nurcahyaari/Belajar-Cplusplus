c602f9ae13d40be1b8aaf74e266fa1a3e4f8e535
Unknown <nurcahyaari@gmail.com>
2017-11-14 20:47:00 +0700

    menambahkan source latihan

---
+++ source latihan/menghitung_luas_lingkaran.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float r, hasil;
+    const float pi = 3.14;
+    r = 7;
+    hasil = pi * r * r;
+
+    cout << "Luas lingkarannya adalah : " << luas << endl;
+}
